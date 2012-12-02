0d6305fe7499eca6562fffd93e4214befc526206
David Jonas <davidjonasdesign@gmail.com>
2012-12-03 00:38:46 +0100

    Adding all the geolocation and design stuff. No youtube videos yet.

---
+++ js/MediaHunt.js
@@ -0,0 +1,182 @@
+var MediaHunt = {};
+
+MediaHunt.lat = 0;
+MediaHunt.lon = 0;
+MediaHunt.gotPosition = false;
+MediaHunt.points = [];
+MediaHunt.distances = [];
+
+MediaHunt.init = function ()
+{
+    $(".title").fadeIn(2000);
+    window.scrollTo(0,1);
+    if (navigator.geolocation)
+    {
+        MediaHunt.posId = navigator.geolocation.watchPosition(MediaHunt.handleLocation, MediaHunt.posError ,{ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true });
+    }
+    else
+    {
+        $(".title").text("Geolocation is not supported by this browser.");
+        $(".title").slabText();
+    }
+};
+
+MediaHunt.posError = function (error)
+{
+    //alert('There was an error gathering your location data. Sorry! : ' + error.message);
+}
+
+MediaHunt.startGame = function ()
+{
+    $(".title").fadeOut(500);
+    setTimeout(function () {
+        MediaHunt.showMap();
+        MediaHunt.getPointsFromCartoDb();
+    }, 500)
+}
+
+MediaHunt.getPointsFromCartoDb = function ()
+{
+    var sql = "SELECT name, description, ST_AsGeoJSON(the_geom) as location FROM media_points";
+    $.getJSON("http://davidjonas.cartodb.com/api/v2/sql?q=" + sql, function(data){
+		var rows = data["rows"];
+                $.each(rows, function (index, row) {
+                        MediaHunt.points.push(row);
+                })
+	});
+}
+
+MediaHunt.showMap = function ()
+{
+    //var map = $("<iframe id='map' src='https://davidjonas.cartodb.com/tables/media_points/embed_map'></iframe>").css({'width':'100%', 'height':$(document).height()});
+    //$('body').append(map);
+
+     
+    
+    var user  = "davidjonas";
+    var table = "media_points";
+    var lat   = MediaHunt.lat;
+    var lng   = MediaHunt.lon;
+    var zoom  = 18;
+    var mapdiv = $('<div id="map"></div>').css({'width':'100%', 'height':$(document).height()});
+    $('body').append(mapdiv);
+    
+    // Define the initial options
+    var cartodbMapOptions = {
+      zoom: zoom,
+      center: new google.maps.LatLng( lat, lng ),
+      disableDefaultUI: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    
+    // Initialize the map
+    MediaHunt.map = new google.maps.Map(document.getElementById("map"),cartodbMapOptions);
+    
+    // Define the map styles
+    var map_style = [{
+      stylers: [{ saturation: -65 }, { gamma: 1.52 }] }, {
+      featureType: "administrative", stylers: [{ saturation: -95 }, { gamma: 2.26 }] }, {
+      featureType: "water", elementType: "labels", stylers: [{ visibility: "off" }] }, {
+      featureType: "administrative.locality", stylers: [{ visibility: 'off' }] }, {
+      featureType: "road", stylers: [{ visibility: "simplified" }, { saturation: -99 }, { gamma: 2.22 }] }, {
+      featureType: "poi", elementType: "labels", stylers: [{ visibility: "off" }] }, {
+      featureType: "road.arterial", stylers: [{ visibility: 'off' }] }, {
+      featureType: "road.local", elementType: "labels", stylers: [{ visibility: 'off' }] }, {
+      featureType: "transit", stylers: [{ visibility: 'off' }] }, {
+      featureType: "road", elementType: "labels", stylers: [{ visibility: 'off' }] }, {
+      featureType: "poi", stylers: [{ saturation: -55 }]
+    }];
+    
+    // Set the style
+    MediaHunt.map.setOptions({ styles: map_style });
+    
+    // Define the layer
+    var cartoDBLayer = {
+      getTileUrl: function(coord, zoom) {
+        return "https://" + user + ".cartodb.com/tiles/" + table + "/" + zoom + "/" + coord.x + "/" + coord.y + ".png";
+      },
+      tileSize: new google.maps.Size(256, 256)
+    };
+    
+    // Add the CartoDB tiles
+    MediaHunt.map.overlayMapTypes.insertAt(0, new google.maps.ImageMapType(cartoDBLayer));
+    
+    var latlon = new google.maps.LatLng(lat, lng);
+    MediaHunt.marker = new google.maps.Marker({position:latlon,map:MediaHunt.map,title:"You are here."});
+    MediaHunt.marker.setIcon('geomarker.png');
+}
+
+MediaHunt.handleLocation = function (position)
+{
+    MediaHunt.lat = position.coords.latitude;
+    MediaHunt.lon = position.coords.longitude;
+    
+    if (MediaHunt.gotPosition == false)
+    {
+        MediaHunt.startGame();
+        MediaHunt.gotPosition = true;
+    }
+    else
+    {
+        MediaHunt.updatePosition();
+    }
+    MediaHunt.recalculateDistances();
+}
+
+MediaHunt.updatePosition = function ()
+{
+    MediaHunt.marker.setMap(null);
+    var latlon = new google.maps.LatLng(MediaHunt.lat, MediaHunt.lon);
+    MediaHunt.marker = new google.maps.Marker({position:latlon,map:MediaHunt.map,title:"You are here."});
+    MediaHunt.marker.setIcon('geomarker.png');
+}
+
+MediaHunt.recalculateDistances = function ()
+{
+    //var sql = "SELECT name, description, ST_AsGeoJSON(the_geom) as location FROM media_points WHERE ST_distance_sphere(the_geom, ST_Point("+MediaHunt.lat+", "+MediaHunt.lon+")) < 20 LIMIT 1";
+    
+    var sql = "SELECT name, description, ST_AsGeoJSON(the_geom) as location FROM media_points WHERE ST_Intersects( the_geom, ST_Buffer( ST_SetSRID('POINT(" + MediaHunt.lon + " " + MediaHunt.lat + ")'::geometry , 4326),"+MediaHunt.getRadDeg(20)+"))";
+    
+    $.getJSON("http://davidjonas.cartodb.com/api/v2/sql?q=" + sql, function (data)
+	      {
+		    if (data.rows.length == 1)
+		    {
+			MediaHunt.playMedia(data.rows[0])
+		    }
+	      })
+}
+
+MediaHunt.playMedia = function (point)
+{
+    alert("Great!!! You found "+ point.name);
+}
+
+MediaHunt.getRadDeg = function(dist)
+{
+    var brng = 180 * Math.PI / 180;
+    var distance = dist/6371000;
+    var lat1 = MediaHunt.lat * Math.PI / 180;
+    var lon1 = MediaHunt.lon * Math.PI / 180;
+
+    var lat2 = Math.asin(Math.sin(lat1) * Math.cos(distance) + 
+               Math.cos(lat1) * Math.sin(distance) * Math.cos(brng));
+
+    var lon2 = lon1 + Math.atan2(Math.sin(brng) * Math.sin(distance) * Math.cos(lat1), Math.cos(distance) - 
+               Math.sin(lat1) * Math.sin(lat2));
+
+    if (isNaN(lat2) || isNaN(lon2)) return null;
+
+    return MediaHunt.lat - (lat2 * 180 / Math.PI);
+}
+
+
+MediaHunt.resize = function () {
+    $('#map').css({'width':'100%', 'height':$(document).height()});
+}
+
+$(function (){
+    MediaHunt.init();
+    window.onresize = function(event) {
+        MediaHunt.resize(event);
+    }
+});
